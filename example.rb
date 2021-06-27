c3de003acf3539d1ac92bb311e86cf6f65fe9b24
Masato516 <yaginen9@gmail.com>
2021-06-28 01:22:47 +0900

    Rubyの例コードを追加

--- example.rb
+++ example.rb
@@ -1,9 +1,158 @@
 
+# CSVライブラリを読み込む
+require 'csv'
+# 入力値を取得
+output_type = ARGV[0]
+file_name   = ARGV[1]
+# 科目数
+subjects_num = 6
+
+# 落第者のIDを出力する場合
+if output_type == "dropouts"
+  puts "ID" # ヘッダー
+  # CSVデータの各行を出力
+  CSV.foreach(file_name, headers: true) do |csv|
+    # 点数をINTに変換し、scoreに代入
+    scores = csv[1..6].map(&:to_i)
+    # 49点以下の数を取得
+    failure_num = scores.each.count{|n| n <= 49}
+    if failure_num >= 2 #49点以下のテストが２つ以上ある時
+      puts csv["ID"]
+    end
+  end
+
+# 最高平均点と最低平均点をとった学生のIDを出力
+elsif output_type == "top-vs-bottom"
+  high_score = 0 # 最高点を入れるための変数
+  low_score = 100 # 最低点を入れるための変数
+  mean_scores_table = [] # IDと平均値を入れる配列
+  # CSVデータの各行を出力
+  CSV.foreach(file_name, headers: true) do |csv|
+    id = csv["ID"]
+    # 点数をINTに変換し、各学生のテストの平均値を算出
+    mean_score = csv[1..6].map(&:to_i).sum.fdiv(subjects_num)
+    # 最高点を取得(今までの平均値で最も大きい値を代入)
+    high_score = mean_score if mean_score > high_score
+    # 最低点を取得(今までの平均値で最も小さい値を代入)
+    low_score = mean_score if mean_score < low_score
+    # IDと平均値を配列に追加
+    mean_scores_table.append([id, mean_score])
+  end
+  # 答えを入れるための配列
+  answers = []
+  # idと平均値が入っている二次元配列の中で、最高点と最低点を持つidと点数をanswersに挿入
+  mean_scores_table.each do |arry|
+    id = arry[0]
+    mean_score = arry[1]
+    if mean_score == high_score 
+      answers.append([id, high_score])
+    elsif mean_score == low_score
+      answers.prepend([id, low_score])
+    end
+  end
+  # 答えの標準出力
+  puts "ID,Mean"
+  answers.each do |answer|
+    puts "#{answer[0]},#{sprintf("%2.2f", answer[1])}"
+  end
+end
 
 
+# n    = gets.to_i  # 与えられる数値の数
+# minv = gets.to_i  # １つ目の数値を代入
+# num = gets.to_i  # 2つ目の数値を代入
+# maxDiff = num - minv  # 2つ目の数値と１つ目の数値の差
+
+# minv = num if num < minv  # １つ目の最小値を求める処理をおこなう
+
+# (n - 2).times {
+#   num = gets.to_i
+#   # 現在の値と前の値の中で最小値を引いて、今までの差でも最も大きい値を代入
+#   maxDiff = num - minv if (num - minv) > maxDiff
+#   # 現在まで出てきた値の中で最も小さい値を代入
+#   minv = num           if num < minv
+# }
+
+# puts maxDiff
 
 
+# n    = gets.to_i
+# minv = gets.to_i
+# input_line = gets.to_i
+# maxDiff = input_line - minv
 
+# i = 0
+# while i < n - 2
+#   input_line = gets.to_i
+#   # 現在の値と前の値の中で最小値を引いて、今までの差でも最も大きい値を代入
+#   maxDiff = input_line - minv if (input_line - minv) > maxDiff
+#   # 現在まで出てきた値の中で最も小さい値を代入
+#   minv = input_line        if input_line < minv
+#   i += 1
+# end
+
+# puts maxDiff
+
+# puts maxv - minv
+
+# for j が 1 から n-1 まで
+#   for i が 0 から j-1 まで
+#     maxv = (maxv と R[j]-R[i] のうち大きい方)
+
+
+# minv = R[0]
+# for j が 1 から n-1 まで
+#   maxv = (maxv と R[j]-minv のうち大きい方)
+#   minv = (minv と R[j] のうち小さい方)
+
+
+# def isPrime(num)
+#   if num == 2   # 2 は素数
+#     return true
+#   end
+
+#   if num < 2 || (num % 2 == 0)  # 2未満 かつ 偶数であれば合成数
+#     return false
+#   end
+#   # 3以上の場合、以下の処理を行なう
+#   i = 3
+#   # 合成数は p <= √x を満たす素因数p をもつ という性質を利用
+#   while i <= Math.sqrt(num)
+#     # 割り切れたら、合成数
+#     if num % i == 0
+#       return false
+#     end
+#     # 偶数は合成数なので、奇数のみで割る
+#     i += 2
+#   end
+#   # 3以上で割り切れない数値は素数が確定する
+#   return true
+# end
+
+# n = gets.chomp.to_i
+# primeNum = 0
+# i = 0
+# while i < n
+#   input_line = gets.chomp.to_i
+
+#   if isPrime(input_line)
+#     primeNum += 1
+#   end
+#   i += 1
+# end
+
+# puts primeNum
+
+# insertionSort(A, N) # N個の要素を含む0-オリジンの配列A
+#   for  i が 1 から N-1 まで  # 未ソートの部分列の先頭を示すループ変数
+#     v = A[i]    # A[i]の値を一時的に保持しておくための変数
+#     j = i - 1   # ソート済み部分列からvを挿入するための位置を探すループ変数
+
+#     # i-1 番目に要素があり、かつi-1番目よりi番目の要素が小さいときはループ処理
+#     while j >= 0 かつ A[j] > v
+#       A[j+1] = A[j]   # 
+#       j--
+#     A[j+1] = v
 
 # arry = [500, 100, 50, 10, 5, 1]
 
