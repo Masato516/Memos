36afdda26cb9cb831d3ef734944f90deae38c03d
Masato516 <yaginen9@gmail.com>
2021-07-31 18:15:39 +0900

    Rails雑記メモ作成、PV・UU数の出力関係

---
+++ Rails雑記メモ.rb
@@ -0,0 +1,28 @@
+
+Job.where(company_id: current_company.id).each do |job|
+  p job.access_reports.sum(:page_view)
+end
+
+current_company.jobs.each do |job|
+  p JobAccessReport.where(job_id: job.id).sum(:page_view)
+end
+
+current_company.jobs.each do |job|
+  p Job.job_access_reports
+end
+
+current_company.jobs.first.access_reports.sum(:page_view)
+
+current_company.jobs.order("created_at DESC").page(params[:page]).map do |job|
+  # @jobs_page_view = job.access_reports.calculate
+  {
+    page_view: job.access_reports.sum(:page_view), 
+    unique_user_count: job.access_reports.sum(:unique_user_count)
+  }
+end
+
+job.access_reports.sum(:page_view)
+
+@jobs.each do |job|
+  job.page_view = 1
+end
