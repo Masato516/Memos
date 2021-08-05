bd3d5b28b7c03e0ea346c2f1c354bebeb966496a
Masato516 <yaginen9@gmail.com>
2021-08-05 13:25:56 +0900

    PV・UU数取得の草案

--- Rails雑記メモ.rb
+++ Rails雑記メモ.rb
@@ -36,3 +36,23 @@ end
 JobAccessReport.where(job_id: current_company.jobs).group(:job_id).sum(:page_view)
 
 JobAccessReport.where(job_id: 1..2).sum(:page_view)
+
+
+# 取得したクエリの数値毎によって出力する月を変える
+def show
+  # クエリパラメータ(形式:20XX-0X)で表示する年月の情報を受け取っている
+  @first_day = Date.strptime(params[:date], '%Y-%m') # 表示する月の初日
+  @last_day = @first_day.end_of_month # 表示する月の最終日
+  @prev_month = @first_day.last_month # 表示する月の前の月
+  @next_month = @first_day.next_month # 表示する月の次の月
+  
+  @job = current_company.jobs.find(params[:job_id]) 
+  @job_page_views         = @job.access_reports
+                                .where(reported_on: @first_day..@last_day)
+                                .group(:reported_on)
+                                .sum(:page_view)
+  @job_unique_user_counts = @job.access_reports
+                                .where(reported_on: @first_day..@last_day)
+                                .group(:reported_on)
+                                .sum(:unique_user_count)
+end
